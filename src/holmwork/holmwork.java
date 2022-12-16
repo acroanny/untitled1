f8f48206355087714225b020e010beeb36e54a1c
acroanny <annatkachenkoq@gmail.com>
2022-12-16 21:34:43 +0000

    holmwork

---
+++ src/holmwork/holmwork.java
@@ -0,0 +1,37 @@
+package holmwork;
+
+public class holmwork {
+        public static void main(String[] args){
+
+            byte a1 = 1;
+            short a2 = 2000;
+            int a3 = 1000000;
+            char a4 = 's';
+            float a5 = 12.345f;
+            double a6 = 5;
+            boolean a7 = true;
+            long a8 = 100L;
+
+            System.out.println("bute="+a1);
+            System.out.println("short="+a2);
+            System.out.println("int="+a3);
+            System.out.println("char="+a4);
+            System.out.println("float="+a5);
+            System.out.println("double="+a6);
+            System.out.println("boolean="+a7);
+            System.out.println("long="+a8);
+
+
+            int x=256;
+            System.out.println(x/100);
+            System.out.println(x-251);
+            System.out.println(x-250);
+
+            int b= 742;
+            System.out.println((b/100)%10);
+            System.out.println((b/10)%10);
+            System.out.println((b)%10);
+        }
+
+    }
+
